53a70e2e77050fde828e4a84d7ad8095d544620a
Dmitry Mayer <dmitry.mayer@libertexgroup.com>
2020-03-25 12:24:03 +0100

    added missing method

--- src/main/scala/ru/tinkoff/gatling/kafka/request/builder/KafkaRequestBuilderBase.scala
+++ src/main/scala/ru/tinkoff/gatling/kafka/request/builder/KafkaRequestBuilderBase.scala
@@ -11,4 +11,7 @@ case class KafkaRequestBuilderBase(requestName: Expression[String]) {
   def send[V](payload: Expression[V])(implicit sender: Sender[Nothing, V]): RequestBuilder[_, V] =
     sender.send(requestName, None, payload, HashMap())
 
+  def send[K, V](key: Expression[K], payload: Expression[V], headers: Map[String, String])(implicit sender: Sender[K, V]): RequestBuilder[K, V] =
+    sender.send(requestName, Some(key), payload, headers)
+
 }
