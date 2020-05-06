66cec1728c1f329d9dce73f3eaea048d976d23c5
Dmitry Mayer <dmitry.mayer@libertexgroup.com>
2020-05-06 17:26:17 +0200

    added missing method in dsl

--- src/main/scala/ru/tinkoff/gatling/kafka/request/builder/KafkaRequestBuilderBase.scala
+++ src/main/scala/ru/tinkoff/gatling/kafka/request/builder/KafkaRequestBuilderBase.scala
@@ -5,11 +5,14 @@ import scala.collection.immutable.HashMap
 
 case class KafkaRequestBuilderBase(requestName: Expression[String]) {
 
-  def send[K, V](key: Expression[K], payload: Expression[V])(implicit sender: Sender[K, V]): RequestBuilder[K, V] =
-    sender.send(requestName, Some(key), payload, HashMap())
-
   def send[V](payload: Expression[V])(implicit sender: Sender[Nothing, V]): RequestBuilder[_, V] =
     sender.send(requestName, None, payload, HashMap())
+
+  def send[V](payload: Expression[V], headers: Map[String, String])(implicit sender: Sender[Nothing, V]): RequestBuilder[_, V] =
+    sender.send(requestName, None, payload, headers)
+
+  def send[K, V](key: Expression[K], payload: Expression[V])(implicit sender: Sender[K, V]): RequestBuilder[K, V] =
+    sender.send(requestName, Some(key), payload, HashMap())
 
   def send[K, V](key: Expression[K], payload: Expression[V], headers: Map[String, String])(implicit sender: Sender[K, V]): RequestBuilder[K, V] =
     sender.send(requestName, Some(key), payload, headers)
